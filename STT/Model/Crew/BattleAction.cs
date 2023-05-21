a55306ea1a3199e666365940813b7f59e6fbb897
Nathaniel Moschkin <nathaniel.moschkin@precise-soft.com>
2023-05-21 15:35:22 -0400

    worked out the labels

--- STT/Model/Crew/BattleAction.cs
+++ STT/Model/Crew/BattleAction.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace STT.Model.Crew
 {
@@ -7,6 +8,15 @@ namespace STT.Model.Crew
     /// </summary>
     public class BattleAction
     {
+        public static readonly Dictionary<int, string> TypeMap = new Dictionary<int, string>
+        {
+            { 0, "Attack" },
+            { 1, "Evasion" },
+            { 2, "Accuracy" },
+		    // These are only for penalty
+		    { 3, "Shield Regeneration" }
+        };
+
         /// <summary>
         /// bonus_amount
         /// </summary>
@@ -99,13 +109,24 @@ namespace STT.Model.Crew
 
         public override string ToString()
         {
-            return Symbol;
+            return $"{Name} (Boosts {TypeMap[BonusType]} By {BonusAmount})" + (((ChargePhases?.Length ?? 0) > 0) ? $" ({ChargePhases.Length} Charge Phases)" : "");
         }
 
     }
 
+
+
     public class Penalty
     {
+        public static readonly Dictionary<int, string> TypeMap = new Dictionary<int, string>
+        {
+            { 0, "Attack" },
+            { 1, "Evasion" },
+            { 2, "Accuracy" },
+		    // These are only for penalty
+		    { 3, "Shield Regeneration" }
+        };
+
         /// <summary>
         /// type
         /// </summary>
@@ -118,28 +139,119 @@ namespace STT.Model.Crew
         [JsonProperty("amount")]
         public int Amount { get; set; }
 
+        public override string ToString()
+        {
+            if (TypeMap.ContainsKey(Type))
+            {
+                return $"Decrease {TypeMap[Type]} by {Amount}";
+            }
+
+            return base.ToString();
+        }
+
+        /* 
+         * 
+         
+         
+        
+	static readonly REWARDS_ITEM_TYPE: { [index: number]: string } = {
+		0: 'None',
+		1: 'Crew',
+		2: 'Equipment',
+		3: 'Component',
+		4: 'Shuttle consumable',
+		5: 'Ship part',
+		6: 'Shuttle token',
+		7: 'Crew experience training',
+		8: 'Ship schematic',
+		9: 'Replicator ration',
+		10: 'Honorable citation',
+		11: 'Buff',
+		12: 'Starbase component',
+		13: 'Voyage consumable'
+	};
+
+	static readonly CREW_SHIP_BATTLE_BONUS_TYPE: { [index: number]: string } = {
+		0: 'Attack',
+		1: 'Evasion',
+		2: 'Accuracy',
+		// These are only for penalty
+		3: 'Shield Regeneration'
+	};
+
+	static readonly CREW_SHIP_BATTLE_TRIGGER: { [index: number]: string } = {
+		0: 'None',
+		1: 'Position',
+		2: 'Cloak',
+		4: 'Boarding'
+	};
+
+	static readonly CREW_SHIP_BATTLE_ABILITY_TYPE: { [index: number]: string } = {
+		0: 'Increases bonus boost by +%VAL%',
+		1: 'Immediately deals %VAL%% damage',
+		2: 'Immediately repairs Hull by %VAL%%',
+		3: 'Immediately repairs Shields by %VAL%%',
+		4: '+%VAL% to Crit Rating',
+		5: '+%VAL% to Crit Bonus',
+		6: '+%VAL% to Shield Regeneration',
+		7: '+%VAL%% to Attack Speed',
+		8: 'Increases boarding damage by %VAL%%'
+	};
+
+
+         */
+
+        public static implicit operator int(Penalty penalty)
+        {
+            return penalty.Type;
+        }
     }
 
-    public class Ability
+    public class Ability : Penalty
     {
+        public static readonly Dictionary<int, string> TriggerMap = new Dictionary<int, string>
+        {
+            { 0, "None" },
+            { 1, "Position" },
+            { 2, "Cloak" },
+		    { 4, "Boarding" }
+        };
+
+        new public static readonly Dictionary<int, string> TypeMap = new Dictionary<int, string>
+        {
+            { 0, "Increases bonus boost by +%VAL%" }, // IncreaseBoost
+            { 1, "Immediately deals %VAL%% damage" }, // DealDamage
+            { 2, "Immediately repairs Hull by %VAL%%" }, // RepairHull
+            { 3, "Immediately repairs Shields by %VAL%%" }, // RepairShields
+            { 4, "+%VAL% to Crit Rating" }, // CritRating
+            { 5, "+%VAL% to Crit Bonus" }, // CritBonus
+            { 6, "+%VAL% to Shield Regeneration" }, // ShieldRegeneration
+            { 7, "+%VAL%% to Attack Speed" }, // AttackSpeed
+            { 8, "Increases boarding damage by %VAL%%" } // BoardingDamage
+        };
+
         /// <summary>
         /// condition
         /// </summary>
         [JsonProperty("condition")]
         public int Condition { get; set; }
 
-        /// <summary>
-        /// type
-        /// </summary>
-        [JsonProperty("type")]
-        public int Type { get; set; }
+        public override string ToString()
+        {
+            if (TypeMap.ContainsKey(Type) && TriggerMap.ContainsKey(Condition))
+            {
+                if (Condition != 0)
+                {
+                    return $"{TypeMap[Type].Replace("%VAL%", Amount.ToString())} (Trigger: {TriggerMap[Condition]})";
+                }
+                else
+                {
+                    return $"{TypeMap[Type].Replace("%VAL%", Amount.ToString())}";
+                }
+            }
 
-        /// <summary>
-        /// amount
-        /// </summary>
-        [JsonProperty("amount")]
-        public int Amount { get; set; }
-
+            return base.ToString();
+        }
     }
 
     public class ChargePhase
